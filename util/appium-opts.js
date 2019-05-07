94fef6360334a0577c82c8f106333618d30b83c0
Jan Hellar <jhellar@redhat.com>
2019-05-07 09:58:09 +0200

    feat: test identification in browserstack

--- util/appium-opts.js
+++ util/appium-opts.js
@@ -2,12 +2,18 @@ const opts = {
   hostname:'hub-cloud.browserstack.com',
   logLevel: 'error',
   capabilities: {
-    'device' : 'Google Pixel',
-    'browserstack.user' : process.env.BROWSERSTACK_USER,
-    'browserstack.key' : process.env.BROWSERSTACK_KEY,
-    'app' : process.env.BROWSERSTACK_APP,
+    'os_version': '8.0',
+    'device': 'Google Pixel',
+    'real_mobile': 'true',
+    'project': 'AeroGear Integration Tests',
+    'build': process.env.GIT_COMMIT_DESC,
+    'name': 'device_security_test',
+    'browserstack.local': 'false',
+    'browserstack.user': process.env.BROWSERSTACK_USER,
+    'browserstack.key': process.env.BROWSERSTACK_KEY,
+    'app': process.env.BROWSERSTACK_APP,
     'autoWebview': true,
-    'browserstack.appium_version' : '1.9.1'
+    'browserstack.appium_version': '1.9.1'
   }
 };
 
