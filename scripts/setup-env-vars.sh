2089179622bffcd1dc10f29adf7e0105bac7e4cb
Jan Hellar <jhellar@redhat.com>
2019-06-02 14:15:07 +0200

    fix ci env vars

--- scripts/setup-env-vars.sh
+++ scripts/setup-env-vars.sh
@@ -1,10 +1,10 @@
-export KEYCLOAK_HOST=bs-local.com
+export KEYCLOAK_HOST=$([ "$CI" == "true" ] && echo "keycloak" || echo "bs-local.com")
 export KEYCLOAK_PORT=8080
 export SYNC_HOST=bs-local.com
 export SYNC_PORT=4000
 export METRICS_HOST=bs-local.com
 export METRICS_PORT=3000
-export PGHOST=localhost
+export PGHOST=$([ "$CI" == "true" ] && echo "postgres" || echo "localhost")
 export PGUSER=postgresql
 export PGPASSWORD=postgres
 export PGDATABASE=aerogear_mobile_metrics
