f045369847d0e591b51b99ed2f5f96e846bad594
Davide Bizzarri <david3bizzarri@gmail.com>
2019-06-05 12:57:35 +0200

    send test notification using unifiedpush-node-sender

--- test/push/index.js
+++ test/push/index.js
@@ -1,9 +1,57 @@
-const expect = require('chai').expect
-
+const expect = require('chai').expect;
+const axios = require('axios');
 const mobileServices = require('../../fixtures/mobile-services');
+const sender = require("unifiedpush-node-sender");
 
 describe('Auth', function () {
   this.timeout(0);
+
+  let pushApplicationID;
+  let masterSecret;
+
+  before("create ups application", async () => {
+    const serverKey = process.env.FIREBASE_SERVER_KEY;
+    const senderId = process.env.FIREBASE_SENDER_ID;
+
+    const config = mobileServices.services.find(service => service.name === 'ups');
+
+    // create test application
+    const application = await axios({
+      method: 'post',
+      url: `http://localhost:8089/rest/applications`,
+      data: {
+        name: "test",
+      },
+    });
+    pushApplicationID = application.data.pushApplicationID;
+    masterSecret = application.data.masterSecret;
+
+    // create android variant
+    const variant = await axios({
+      method: 'post',
+      url: `http://localhost:8089/rest/applications/${pushApplicationID}/android`,
+      data: {
+        name: "android",
+        googleKey: serverKey,
+        // googleKey: "AAAATRdW_Xs:APA91bE0VC90ktrkUjRg2P8PhlzIlEtPeK1XYGSYYwMB0LbzZaTzUHaKlTQpDpzWTJM3eQdx6n1466ZwXKv19Syf5LENvP03vsMu9424TO9XlwZ9xsWCtDfIUqShS8Cl8iCg4u5iRVNC",
+        projectNumber: senderId,
+        // projectNumber: "331104058747",
+      },
+    });
+
+    // set variant and secret in config
+    config.config.android.variantId = variant.data.variantID;
+    config.config.android.variantSecret = variant.data.secret;
+  })
+
+  after("delete ups application", async () => {
+
+    // delete test application
+    await axios({
+      method: "delete",
+      url: `http://localhost:8089/rest/applications/${pushApplicationID}`
+    });
+  })
 
   it('should recive the test notification', async function () {
 
@@ -52,11 +100,16 @@ describe('Auth', function () {
       });
     });
 
-
-    // // todo: send a notification
+    // send test notification
+    sender({
+      url: "http://localhost:8089",
+      applicationId: pushApplicationID,
+      masterSecret: masterSecret,
+    }).then(client => {
+      client.sender.send({ alert: "test" }, { criteria: { alias: ["alias"] } });
+    })
 
     // wait for the notification
     expect(await message).to.equal("test")
-
   });
 });
