ec83fc23f85fc7784e3919627ee529c78bb20cc1
Jan Hellar <jhellar@redhat.com>
2019-06-21 10:04:37 +0200

    added rest of device security checks

--- test/security/index.js
+++ test/security/index.js
@@ -13,4 +13,90 @@ describe('Device Security', function() {
 
     result.passed.should.equal(true);
   });
+
+  it('should be possible to run `emulator` check', async function() {
+    const result = await client.executeAsync(async done => {
+      const { SecurityService, DeviceCheckType } = window.aerogear.agSecurity;
+      const securityService = new SecurityService();
+  
+      const result = await securityService.check(DeviceCheckType.isEmulator);
+  
+      done(result);
+    });
+
+    result.passed.should.equal(false);
+  });
+
+  it('should be possible to run `debugMode` check', async function() {
+    const result = await client.executeAsync(async done => {
+      const { SecurityService, DeviceCheckType } = window.aerogear.agSecurity;
+      const securityService = new SecurityService();
+  
+      const result = await securityService.check(DeviceCheckType.debugModeEnabled);
+  
+      done(result);
+    });
+
+    result.passed.should.equal(true);
+  });
+
+  it('should be possible to run `screenLock` check', async function() {
+    const result = await client.executeAsync(async done => {
+      const { SecurityService, DeviceCheckType } = window.aerogear.agSecurity;
+      const securityService = new SecurityService();
+  
+      const result = await securityService.check(DeviceCheckType.screenLockEnabled);
+  
+      done(result);
+    });
+
+    result.passed.should.equal(false);
+  });
+
+  it('should be possible to run multiple checks', async function() {
+    const result = await client.executeAsync(async done => {
+      const { SecurityService, DeviceCheckType } = window.aerogear.agSecurity;
+      const securityService = new SecurityService();
+
+      const result = await securityService.checkMany(
+        DeviceCheckType.debugModeEnabled,
+        DeviceCheckType.rootEnabled,
+        DeviceCheckType.isEmulator,
+        DeviceCheckType.screenLockEnabled
+      );
+  
+      done(result);
+    });
+
+    result.find(r => r.name === 'Debugger Check').passed.should.equal(true);
+    result.find(r => r.name === 'Rooted Check').passed.should.equal(true);
+    result.find(r => r.name === 'Emulator Check').passed.should.equal(false);
+    result.find(r => r.name === 'Screen Lock Check').passed.should.equal(false);
+  });
+
+  it('should be possible to run custom check', async function() {
+    const result = await client.executeAsync(async done => {
+      class CustomDeviceCheck {
+        get name() {
+          return 'My Custom Check';
+        }
+      
+        check() {
+          return Promise.resolve({
+            name: 'My Custom Check',
+            passed: true
+          });
+        }
+      }
+
+      const { SecurityService } = window.aerogear.agSecurity;
+      const securityService = new SecurityService();
+  
+      const result = await securityService.check(new CustomDeviceCheck());
+        
+      done(result);
+    });
+
+    result.passed.should.equal(true);
+  });
 });
