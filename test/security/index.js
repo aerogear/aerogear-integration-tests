1c3b479a87b5059ddd80e38875b65e7175d3d794
jhellar <jhellar@redhat.com>
2019-06-26 13:36:17 +0200

    added comment for rooted check

--- test/security/index.js
+++ test/security/index.js
@@ -11,6 +11,11 @@ describe('Device Security', function() {
       done(result);
     });
 
+    // From BrowserStack support: "...there are certain changes made in the boot images
+    // of our devices. Due to those changes, a few plugins report the device as rooted
+    // when in reality it is not..."
+    // Because of that BrowserStack Android device we use for testing is reported as rooted
+    // by the plugin we use. So we actually check that Android device is rooted and iOS is not.
     result.passed.should.equal(process.env.MOBILE_PLATFORM !== 'ios');
   });
 });
