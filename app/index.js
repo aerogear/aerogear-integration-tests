a0847e14e8dabfc29a5c41766a900d334f7e6088
Davide <david3bizzarri@gmail.com>
2019-07-03 18:59:12 +0200

    test: expose all node modules using a loop

--- app/index.js
+++ app/index.js
@@ -1,47 +1,10 @@
 import { modules } from "./modules";
-import {
-    __assign,
-    __asyncDelegator,
-    __asyncGenerator,
-    __asyncValues,
-    __await,
-    __awaiter,
-    __decorate,
-    __exportStar,
-    __extends,
-    __generator,
-    __importDefault,
-    __importStar,
-    __makeTemplateObject,
-    __metadata,
-    __param,
-    __read,
-    __rest,
-    __spread,
-    __value,
-    __values,
-} from "tslib";
+import * as helpers from "tslib";
 
+// expose all typescript helpers globally
+for (const key in helpers) {
+    window[key] = helpers[key];
+}
 
-window.__assign = __assign;
-window.__asyncDelegator = __asyncDelegator;
-window.__asyncGenerator = __asyncGenerator;
-window.__asyncValues = __asyncValues;
-window.__await = __await;
-window.__awaiter = __awaiter;
-window.__decorate = __decorate;
-window.__exportStar = __exportStar;
-window.__extends = __extends;
-window.__generator = __generator;
-window.__importDefault = __importDefault;
-window.__importStar = __importStar;
-window.__makeTemplateObject = __makeTemplateObject;
-window.__metadata = __metadata;
-window.__param = __param;
-window.__read = __read;
-window.__rest = __rest;
-window.__spread = __spread;
-window.__value = __value;
-window.__values = __values;
-
-window.modules = modules;
+// expose modules globally
+window["modules"] = modules;
