fbaf5e64bdbe95a8841530225f592ad0b2c5da56
Davide <david3bizzarri@gmail.com>
2019-07-24 12:14:02 +0200

    chore: install aerogear-sync plugin

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -10,4 +10,5 @@ cordova plugin add --nosave \
     @aerogear/cordova-plugin-aerogear-metrics@dev \
     @aerogear/cordova-plugin-aerogear-push@dev \
     @aerogear/cordova-plugin-aerogear-security@dev \
+    @aerogear/cordova-plugin-aerogear-sync@dev \
     @aerogear/cordova-plugin-iroot@latest
