1234845d4a116dbc6348c0ca3007eb258d5359a5
Davide <david3bizzarri@gmail.com>
2019-08-06 19:38:25 +0200

    refactor: enforce types on sync test

--- test/sync/index.ts
+++ test/sync/index.ts
@@ -1,19 +1,31 @@
-require("chai").should();
-const express = require("express");
-const { VoyagerServer, gql } = require("@aerogear/voyager-server");
+import chai = require("chai");
+chai.should();
 
-// @ts-ignore
-const { setNetwork } = require("../../util/network");
+import { ApolloOfflineClient } from "@aerogear/voyager-client";
+import { gql, VoyagerServer } from "@aerogear/voyager-server";
+import express = require("express");
+import { CordovaNetworkStatus } from "offix-offline";
+import { ToggleNetworkStatus } from "../../testing-app/ToggleNetworkStatus";
+import { device } from "../../util/device";
+import { GlobalUniverse } from "../../util/init";
+import { setNetwork } from "../../util/network";
+
+interface Universe extends GlobalUniverse {
+    networkStatus: ToggleNetworkStatus | CordovaNetworkStatus;
+    itemsQuery: any;
+    apolloClient: ApolloOfflineClient;
+    offlineChangePromise: Promise<any>;
+}
 
 describe("Data Sync", function() {
     this.timeout(0);
 
     let expressServer;
-    let serverItems = [];
+    const serverItems = [];
     let numItems = 0;
 
-    it("should run the voyager server", async function() {
-        const typeDefs = gql`
+    it("should run the voyager server", async () => {
+        const typeDefs = gql(`
             type Item {
                 id: ID!
                 title: String!
@@ -26,7 +38,7 @@ describe("Data Sync", function() {
             type Mutation {
                 create(title: String!): Item
             }
-        `;
+        `);
 
         const resolvers = {
             Query: {
@@ -47,10 +59,13 @@ describe("Data Sync", function() {
             },
         };
 
-        const server = VoyagerServer({
-            typeDefs,
-            resolvers,
-        });
+        const server = VoyagerServer(
+            {
+                typeDefs,
+                resolvers,
+            },
+            {}
+        );
 
         const app = express();
         server.applyMiddleware({ app });
@@ -58,101 +73,85 @@ describe("Data Sync", function() {
         expressServer = app.listen(4000);
     });
 
-    it("should initialize voyager client", async function() {
-        // @ts-ignore
-        await client.executeAsync(async (platform, done) => {
-            try {
-                const {
-                    app,
-                    agSync: { OfflineClient, CordovaNetworkStatus },
-                    gql,
-                    ToggleNetworkStatus,
-                    offixCache: { CacheOperation, getUpdateFunction },
-                    // @ts-ignore
-                } = window.aerogear;
+    it("should initialize voyager client", async () => {
+        await device.execute(async (modules, universe: Universe, platform) => {
+            const { OfflineClient, CordovaNetworkStatus } = modules[
+                "@aerogear/voyager-client"
+            ];
+            const { gql } = modules["graphql-tag"];
+            const { ToggleNetworkStatus } = modules["./ToggleNetworkStatus"];
+            const { CacheOperation, getUpdateFunction } = modules[
+                "offix-cache"
+            ];
 
-                let networkStatus;
+            const { app } = universe;
 
-                if (platform === "ios") {
-                    // this is workaround for iOS as BrowserStack does not support
-                    // putting iOS devices offline
-                    networkStatus = new ToggleNetworkStatus();
-                } else {
-                    networkStatus = new CordovaNetworkStatus();
-                }
+            let networkStatus;
 
-                // @ts-ignore
-                window.aerogear.networkStatus = networkStatus;
-
-                const itemsQuery = gql`
-                    query items {
-                        items {
-                            id
-                            title
-                        }
-                    }
-                `;
-                // @ts-ignore
-                window.aerogear.itemsQuery = itemsQuery;
-
-                const cacheUpdates = {
-                    create: getUpdateFunction({
-                        mutationName: "create",
-                        idField: "id",
-                        operationType: CacheOperation.ADD,
-                        updateQuery: itemsQuery,
-                    }),
-                };
-
-                const options = {
-                    openShiftConfig: app.config,
-                    networkStatus,
-                    mutationCacheUpdates: cacheUpdates,
-                };
-
-                const offlineClient = new OfflineClient(options);
-
-                const apolloClient = await offlineClient.init();
-
-                // @ts-ignore
-                window.aerogear.apolloClient = apolloClient;
-
-                done();
-            } catch (error) {
-                done({ error: error.message });
+            if (platform === "ios") {
+                // this is workaround for iOS as BrowserStack does not support
+                // putting iOS devices offline
+                networkStatus = new ToggleNetworkStatus();
+            } else {
+                networkStatus = new CordovaNetworkStatus();
             }
+
+            universe.networkStatus = networkStatus;
+
+            const itemsQuery = gql(`
+                query items {
+                    items {
+                        id
+                        title
+                    }
+                }
+            `);
+            universe.itemsQuery = itemsQuery;
+
+            const cacheUpdates = {
+                create: getUpdateFunction({
+                    mutationName: "create",
+                    idField: "id",
+                    operationType: CacheOperation.ADD,
+                    updateQuery: itemsQuery,
+                }),
+            };
+
+            const options = {
+                openShiftConfig: app.config,
+                networkStatus,
+                mutationCacheUpdates: cacheUpdates,
+            };
+
+            const offlineClient = new OfflineClient(options);
+
+            const apolloClient = await offlineClient.init();
+
+            universe.apolloClient = apolloClient;
         }, process.env.MOBILE_PLATFORM);
     });
 
-    it("should perform query", async function() {
-        // @ts-ignore
-        const result = await client.executeAsync(async done => {
-            try {
-                // @ts-ignore
-                const { apolloClient, itemsQuery } = window.aerogear;
+    it("should perform query", async () => {
+        const result = await device.execute(async (_, universe: Universe) => {
+            const { apolloClient, itemsQuery } = universe;
 
-                const { data } = await apolloClient.query({
-                    query: itemsQuery,
-                    fetchPolicy: "network-only",
-                    errorPolicy: "none",
-                });
+            const { data } = await apolloClient.query({
+                query: itemsQuery,
+                fetchPolicy: "network-only",
+                errorPolicy: "none",
+            });
 
-                done({ data: data.items });
-            } catch (error) {
-                done({ error: error.message });
-            }
+            return { data: data.items };
         });
 
         result.data.should.deep.equal([]);
     });
 
-    it("should perform offline mutation", async function() {
+    it("should perform offline mutation", async () => {
         if (process.env.MOBILE_PLATFORM === "ios") {
-            // @ts-ignore
-            client.execute(() => {
-                // @ts-ignore
-                const { networkStatus } = window.aerogear;
-                networkStatus.setOnline(false);
+            await device.execute(async (_, universe: Universe) => {
+                const { networkStatus } = universe;
+                (networkStatus as ToggleNetworkStatus).setOnline(false);
             });
         } else {
             await setNetwork("no-network");
@@ -160,96 +159,75 @@ describe("Data Sync", function() {
 
         await new Promise(resolve => setTimeout(resolve, 5000));
 
-        // @ts-ignore
-        await client.executeAsync(async done => {
+        await device.execute(async (modules, universe: Universe) => {
             try {
-                // @ts-ignore
-                const { apolloClient, gql, itemsQuery } = window.aerogear;
+                const { gql } = modules["graphql-tag"];
+                const { apolloClient, itemsQuery } = universe;
 
                 await apolloClient.offlineMutate({
-                    mutation: gql`
+                    mutation: gql(`
                         mutation create($title: String!) {
                             create(title: $title) {
                                 id
                                 title
                             }
                         }
-                    `,
+                    `),
                     variables: { title: "test" },
                     updateQuery: itemsQuery,
                     returnType: "Item",
                 });
-
-                done({ error: "network error offline was not thrown" });
             } catch (error) {
                 if (error.networkError && error.networkError.offline) {
                     const offlineError = error.networkError;
-                    // @ts-ignore
-                    window.aerogear.offlineChangePromise = offlineError.watchOfflineChange();
-                    done();
+                    universe.offlineChangePromise = offlineError.watchOfflineChange();
                     return;
                 }
 
-                done({ error: error.message });
+                throw error;
             }
+
+            throw new Error("network error offline was not thrown");
         });
     });
 
-    it("should see updated cache", async function() {
-        // @ts-ignore
-        const result = await client.executeAsync(async done => {
-            try {
-                // @ts-ignore
-                const { apolloClient, itemsQuery } = window.aerogear;
+    it("should see updated cache", async () => {
+        const result = await device.execute(async (_, universe: Universe) => {
+            const { apolloClient, itemsQuery } = universe;
 
-                const { data } = await apolloClient.query({
-                    query: itemsQuery,
-                });
+            const { data } = await apolloClient.query({
+                query: itemsQuery,
+            });
 
-                done({ data: data.items });
-            } catch (error) {
-                done({ error: error.message });
-            }
+            return { data: data.items };
         });
 
         result.data.length.should.equal(1);
         result.data[0].title.should.equal("test");
     });
 
-    it("should sync changes when going online", async function() {
+    it("should sync changes when going online", async () => {
         if (process.env.MOBILE_PLATFORM === "ios") {
-            // @ts-ignore
-            client.execute(() => {
-                // @ts-ignore
-                const { networkStatus } = window.aerogear;
-                networkStatus.setOnline(true);
+            await device.execute(async (_, universe: Universe) => {
+                const { networkStatus } = universe;
+                (networkStatus as ToggleNetworkStatus).setOnline(true);
             });
         } else {
             await setNetwork("reset");
         }
 
-        // @ts-ignore
-        const result = await client.executeAsync(async done => {
-            try {
-                const {
-                    apolloClient,
-                    itemsQuery,
-                    offlineChangePromise,
-                    // @ts-ignore
-                } = window.aerogear;
+        const result = await device.execute(async (_, universe: Universe) => {
+            const { apolloClient, itemsQuery, offlineChangePromise } = universe;
 
-                await offlineChangePromise;
+            await offlineChangePromise;
 
-                await new Promise(resolve => setTimeout(resolve, 1000));
+            await new Promise(resolve => setTimeout(resolve, 1000));
 
-                const { data } = await apolloClient.query({
-                    query: itemsQuery,
-                });
+            const { data } = await apolloClient.query({
+                query: itemsQuery,
+            });
 
-                done({ data: data.items });
-            } catch (error) {
-                done({ error: error.message });
-            }
+            return { data: data.items };
         });
 
         result.data.should.deep.equal([
@@ -267,7 +245,7 @@ describe("Data Sync", function() {
         ]);
     });
 
-    it("shouold stop voyager server", async function() {
+    it("should stop voyager server", async () => {
         expressServer.close();
     });
 });
