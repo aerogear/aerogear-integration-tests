21bf9bc936d7a51158a1844565315d860c5fbc94
Davide <david3bizzarri@gmail.com>
2019-08-06 13:24:17 +0200

    chore: add test for the device util

---
+++ test/util/device.ts
@@ -0,0 +1,53 @@
+import { expect } from "chai";
+import { device } from "../../util/device";
+
+// Test the utils library for integration tests
+
+describe("test device test util", function() {
+    this.timeout(0);
+
+    it("store and read from universe", async () => {
+        interface Universe {
+            hello: string;
+        }
+
+        await device.execute(async (_, universe: Universe, hello) => {
+            universe.hello = hello;
+        }, "Hello");
+
+        const result = await device.execute(async (_, universe: Universe) => {
+            return universe.hello;
+        });
+
+        expect(result).to.equal("Hello");
+    });
+
+    it("use a module", async () => {
+        await device.execute(
+            async (modules, _, config) => {
+                const { init } = modules["@aerogear/app"];
+
+                init(config);
+            },
+            { version: 1, namespace: "integration", clusterName: "test" }
+        );
+    });
+
+    it("use await inside the async function", async () => {
+        await device.execute(async () => {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        });
+    });
+
+    it("throw an exception", async () => {
+        let error: Error;
+        try {
+            await device.execute(async () => {
+                throw new Error("a strange exception");
+            });
+        } catch (e) {
+            error = e;
+        }
+        expect(error.message).contain("a strange exception");
+    });
+});
